f41ae8bb613c6b5466714fa09d4d78b853d9c66d
sevoster <sevoster@gmail.com>
2017-04-08 16:49:46 +0300

    Created main window class

--- MainWindow.py
+++ MainWindow.py
@@ -1,13 +1,41 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QAction, qApp
+from PyQt5.QtGui import QIcon
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.statusBar().showMessage('Ready')
+
+        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        self.resize(600, 400)
+        self.center()
+
+        self.setWindowTitle('Main Window')
+        self.show()
+
+    def center(self):
+
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    w = QWidget()
-    w.resize(250, 100)
-    w.move(300, 300)
-    w.setWindowTitle('Main')
-    w.show()
+    mw = MainWindow()
 
     sys.exit(app.exec_())
