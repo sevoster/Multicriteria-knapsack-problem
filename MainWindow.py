41955189ffc5ab9433f571dbe60f4e6cd30e23bc
Polunin, Dmitry <dmitry.polunin@intel.com>
2017-04-17 13:09:19 +0300

    Message box instead of print, added run button

--- MainWindow.py
+++ MainWindow.py
@@ -1,13 +1,18 @@
 import sys
+from PyQt5 import QtCore
 
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QAction, qApp, QFileDialog
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QDesktopWidget, QAction, qApp, QFileDialog, QPushButton,
+                             QGridLayout, QHBoxLayout, QVBoxLayout, QWidget)
 
 import file_parser
 import core
 
 
 class MainWindow(QMainWindow):
+    input_data = []
+
     def __init__(self):
         super().__init__()
         self.init_ui()
@@ -30,6 +35,18 @@ class MainWindow(QMainWindow):
         file_menu.addAction(import_action)
         file_menu.addAction(exit_action)
 
+        central_widget = QWidget(self)
+
+        run_button = QPushButton('Run')
+        run_button.clicked.connect(self.on_run_click)
+
+        v_box_buttons = QVBoxLayout()
+        v_box_buttons.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTop)
+        v_box_buttons.addWidget(run_button)
+
+        central_widget.setLayout(v_box_buttons)
+
+        self.setCentralWidget(central_widget)
         self.resize(600, 400)
         self.center()
 
@@ -44,10 +61,18 @@ class MainWindow(QMainWindow):
 
     def import_file(self):
         file_name = QFileDialog.getOpenFileName(self, 'Open file', 'C:\\', "Text files (*.txt)")[0]
-        input_data = file_parser.parse(file_name)
-        # table = core.gettable(input_data[0], input_data[1], input_data[2][2], input_data[2][0], input_data[2][1])
-        self.statusBar().showMessage('Import File: Success')
-        print(input_data)
+        self.input_data = file_parser.parse(file_name)
+        if self.input_data != -1:
+            self.statusBar().showMessage('Import File: Success')
+        else:
+            self.input_data = []
+            self.statusBar().showMessage('Import File: Error')
+
+    @pyqtSlot()
+    def on_run_click(self):
+        if self.input_data:
+            # Put here code to find solution
+            print(self.input_data)
 
 
 if __name__ == '__main__':
--- file_parser/file_parser.py
+++ file_parser/file_parser.py
@@ -1,7 +1,10 @@
+from PyQt5.QtWidgets import QMessageBox
 
 
 def parse(path):
     file = open(path, 'r')
+    msg = QMessageBox()
+    msg.setWindowTitle("Parse Error")
     try:
         dimension = int(file.readline())
         knapsack_capacity = int(file.readline())
@@ -9,11 +12,15 @@ def parse(path):
         for line in file:
             condition_coefficients.append(list(map(int, line.split(' '))))
     except ValueError:
-        print("File has wrong format: Cannot parse input data.\nNote: Be sure that file contains one empty line in "
-              "the end.")
+        msg.setText("File has wrong format: Cannot parse input data.\nNote: Be sure that file contains one empty line "
+                    "in the end.")
+        msg.setIcon(QMessageBox.Critical)
+        msg.exec_()
         return -1
     if condition_coefficients.__len__() != 3:
-        print("There should be three lines with coefficients in file.\nNote: Be sure that file contains one empty "
-              "line in the end.")
+        msg.setText("There should be three lines with coefficients in file.\nNote: Be sure that file contains one "
+                    "empty line in the end.")
+        msg.setIcon(QMessageBox.Critical)
+        msg.exec_()
         return -1
     return dimension, knapsack_capacity, condition_coefficients[0], condition_coefficients[1], condition_coefficients[2]
